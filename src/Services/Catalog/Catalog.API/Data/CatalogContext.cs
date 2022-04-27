2aec26877c6b5a5bcf6d519066bda3facfc01dbe
mahendrasahu <mahendra.mcsa@gmail.com>
2022-04-27 10:48:08 +0530

    Intial Commit

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var client = new MongoClient(configBuilder.GetSection("DatabaseSettings")["ConnectionString"]);
+            var database = client.GetDatabase(configBuilder.GetSection("DatabaseSettings")["DatabaseName"]);
+            Products = database.GetCollection<Product>(configBuilder.GetSection("DatabaseSettings")["CollectionName"]);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
